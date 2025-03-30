74fe5b66c788127049622265619d497370add796
Miggi <m.famgarcia@outlook.com>
2025-03-30 16:06:07 +0200

    Scroll FUnktion und Desgign

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function renderBooks() {
- // getLocalStorage();
+  // getLocalStorage();
   let contentRef = document.getElementById('content');
   contentRef.innerHTML = "";
 
   for (let indexBook = 0; indexBook < books.length; indexBook++) {
     const book = books[indexBook];
-      contentRef.innerHTML += getBookTemplate(book, indexBook);
+    contentRef.innerHTML += getBookTemplate(book, indexBook);
+    renderComments(indexBook);
   }
 }
 
@@ -14,7 +15,7 @@ function getBookTemplate(book, indexBook) {
     <div class="book_content">
     <div class="bookTitle"><h1>${book.name}</h1></div>
     <div class="imgContent"><img src="./img/book.jpeg" alt=""></div>
-    <div class="bookPrice"><h2><p>Price ${book.price}</p></h2>Likes${book.likes}</div>
+    <div class="bookPrice"><h2><p>Price ${book.price}€</p></h2>Likes${book.likes}</div>
     <div class="bookAuthor">
     <p>${book.author}</p>
     <p>${book.genre}</p>
@@ -22,7 +23,28 @@ function getBookTemplate(book, indexBook) {
     </div>
     <div class="bookLikes"></div>
     <div class="bookLiked">${book.liked}</div>
-    <div class="bookComments">${book.comments}</div>
+    <div id="comments-${indexBook}" class="bookComments">book.comments</div>
     </div>
   `;
- }
+}
+
+function renderComments(indexBook) {
+  const comments = books[indexBook].comments;
+  const commentsContainer = document.getElementById(`comments-${indexBook}`);
+  commentsContainer.innerHTML = "";
+
+  for (let commentsIndex = 0; commentsIndex < comments.length; commentsIndex++) {
+    commentsContainer.innerHTML += `
+    <div class="commentsContent">
+      <div class="commentContent">
+        <h3>${comments[commentsIndex].name}</h3>
+        <p>${comments[commentsIndex].comment}</p>
+      </div>
+    </div>`;
+  }
+}
+
+function like(indexBook) {
+  const like = books[indexBook].liked;
+
+}
